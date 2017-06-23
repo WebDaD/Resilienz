ac9bb0f79ec64fb239c8cebcd554ce83ea5d3e99
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-06-23 11:02:46 +0200

    Create User Done, added addAdminUser to database

--- scripts/createUser.js
+++ scripts/createUser.js
@@ -1,25 +1,73 @@
-// TODO:
-// 1. Ask for Username https://www.npmjs.com/package/readline-sync
-// 2. Ask for password
-// 3. Repeat Passwort
-// 4. Ask if Admin
-// 5. if no admin, ask details for action
-// 6. ask language
-// 7. Hash password md5+bcrypt  bcrypt.hash(dataset.register_password + self.secret, self.salt, callback) https://www.npmjs.com/package/md5
-// 8. use code from lib/login to hash password
-// 9. use lib/database to addUser into db
-// 10. return OK or Error
-
-/*
-SECRET:
-config
-
-SALT:
-var saltfilecontent = fs.readFileSync(self.saltfile)
+var config = require('./config.json')
+var bcrypt = require('bcrypt')
+var fs = require('fs')
+var md5 = require('md5')
+var readlineSync = require('readline-sync')
+var Database = require('./lib/database.js')
+var database = new Database(config.database)
+var salt = ''
+var saltfilecontent = fs.readFileSync(config.salt)
 if (!saltfilecontent) {
-  self.salt = self.bcrypt.genSaltSync(self.saltRounds)
-  fs.writeFileSync(self.saltfile, self.salt)
+  salt = bcrypt.genSaltSync(10)
+  fs.writeFileSync(config.salt, salt)
 } else {
-  self.salt = saltfilecontent
+  salt = saltfilecontent
 }
-*/
+
+console.log('Welcome to the User Creator for Resiliency!')
+var data = {}
+data.register_vorname = readlineSync.question('Enter Vorname: ')
+data.register_nachname = readlineSync.question('Enter Nachname: ')
+data.register_email = readlineSync.questionEMail('Enter E-Mail: ')
+data.register_language = readlineSync.question('Enter LanguageCode [en]: ', {
+  defaultInput: 'en'
+})
+data.password = readlineSync.question('Enter Password: ', {
+  hideEchoBack: true
+})
+data.passwordRepeat = readlineSync.question('Repeat Password: ', {
+  hideEchoBack: true
+})
+while (data.password !== data.passwordRepeat) {
+  console.error('Passwords do not match, please try again (or STRG+C to exit)')
+  data.passwordRepeat = readlineSync.question('Repeat Password: ', {
+    hideEchoBack: true
+  })
+}
+data.admin = 0
+if (readlineSync.keyInYN('Is this an Admin-User? [yn] ')) {
+  data.admin = 1
+} else {
+  data.register_location = readlineSync.question('Enter Location: ')
+  data.register_start = readlineSync.question('Enter Start Time: ')
+  data.register_stop = readlineSync.question('Enter Stop Time: ')
+}
+data.password = md5(data.password)
+bcrypt.hash(data.password + config.secret, salt, function (error, hash) {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  } else {
+    if (data.admin === '1') {
+      database.addAdminUser(data, function (error, result) {
+        if (error) {
+          console.error(error)
+          process.exit(1)
+        } else {
+          console.log('ADMIN User ' + data.email + ' added with ID ' + result.id)
+          process.exit(0)
+        }
+      })
+    } else {
+      database.addUser(data, function (error, result) {
+        if (error) {
+          console.error(error)
+          process.exit(1)
+        } else {
+          console.log('User ' + data.email + ' added with ID ' + result.id)
+          process.exit(0)
+        }
+      })
+    }
+  }
+})
--- scripts/sql/structure.sql
+++ scripts/sql/structure.sql
@@ -259,6 +259,7 @@ CREATE TABLE `user` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(64) NOT NULL,
   `languages_id` int(11) NOT NULL,
+  `admin` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_user_languages1_idx` (`languages_id`),
