ce3596fe557b7b7f0067efdf3ef204ea105b7904
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-08-07 13:15:09 +0200

    Fix of update.sql (wrong table)

--- scripts/sql/layout_symbols.sql
+++ scripts/sql/layout_symbols.sql
@@ -1,11 +1,11 @@
 -- Update Script for Database from Master to layout_symbols
-ALTER TABLE `resilienz-test`.`layouts` ADD COLUMN `symbol` VARCHAR(2) NOT NULL AFTER `name`;
+ALTER TABLE `resilienz`.`layouts` ADD COLUMN `symbol` VARCHAR(2) NOT NULL AFTER `name`;
 
-UPDATE `resilienz-test`.`layouts` SET `symbol`='A' WHERE `id`='1';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='B' WHERE `id`='2';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='C' WHERE `id`='3';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='D' WHERE `id`='4';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='E' WHERE `id`='5';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='F' WHERE `id`='6';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='G' WHERE `id`='7';
-UPDATE `resilienz-test`.`layouts` SET `symbol`='H' WHERE `id`='8';
+UPDATE `resilienz`.`layouts` SET `symbol`='A' WHERE `id`='1';
+UPDATE `resilienz`.`layouts` SET `symbol`='B' WHERE `id`='2';
+UPDATE `resilienz`.`layouts` SET `symbol`='C' WHERE `id`='3';
+UPDATE `resilienz`.`layouts` SET `symbol`='D' WHERE `id`='4';
+UPDATE `resilienz`.`layouts` SET `symbol`='E' WHERE `id`='5';
+UPDATE `resilienz`.`layouts` SET `symbol`='F' WHERE `id`='6';
+UPDATE `resilienz`.`layouts` SET `symbol`='G' WHERE `id`='7';
+UPDATE `resilienz`.`layouts` SET `symbol`='H' WHERE `id`='8';
