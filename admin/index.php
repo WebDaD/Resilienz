0d90f8940da75221333b72d3d24fbe31c315133a
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-09-19 09:08:59 +0200

    admin better mobile table

--- admin/index.php
+++ admin/index.php
@@ -42,12 +42,62 @@
       $sql = "SELECT u.email, u.language, u.action_id, a.finalized, a.book FROM `userList` u, actionList a WHERE u.id = a.user_id";
       $result = $conn->query($sql);
     ?>
+    <style>
+      @media only screen and (max-width: 800px) {
+        /* Force table to not be like tables anymore */
+        #no-more-tables table, 
+        #no-more-tables thead, 
+        #no-more-tables tbody, 
+        #no-more-tables th, 
+        #no-more-tables td, 
+        #no-more-tables tr { 
+          display: block; 
+        }
+      
+        /* Hide table headers (but not display: none;, for accessibility) */
+        #no-more-tables thead tr { 
+          position: absolute;
+          top: -9999px;
+          left: -9999px;
+        }
+      
+        #no-more-tables tr { border: 1px solid #ccc; }
+      
+        #no-more-tables td { 
+          /* Behave  like a "row" */
+          border: none;
+          border-bottom: 1px solid #eee; 
+          position: relative;
+          padding-left: 50%; 
+          white-space: normal;
+          text-align:left;
+        }
+      
+        #no-more-tables td:before { 
+          /* Now like a table header */
+          position: absolute;
+          /* Top/left values mimic padding */
+          top: 6px;
+          left: 6px;
+          width: 45%; 
+          padding-right: 10px; 
+          white-space: nowrap;
+          text-align:left;
+          font-weight: bold;
+        }
+      
+        /*
+        Label the data
+        */
+        #no-more-tables td:before { content: attr(data-title); }
+      }
+    </style>
   </head>
   <body>
     <div class="container">
       <h1>Resilienz :: Books</h1>
       <?php if ($result->num_rows > 0):?>
-      <div class="table-responsive">
+      <div id="no-more-tables">
         <table class="table table-bordered table-striped table-hover sortable">
           <thead>
             <tr>
