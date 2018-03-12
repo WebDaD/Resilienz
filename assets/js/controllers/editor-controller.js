e7da5a817e32fff63301787e17d219dbd8106a32
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-03-12 15:51:08 +0100

    Fixed editor

--- assets/js/controllers/editor-controller.js
+++ assets/js/controllers/editor-controller.js
@@ -25,8 +25,14 @@
           $uibModalInstance.close(data.image)
         })
       }
+      self.coords = {
+        x1: 1,
+        y1: 1,
+        x2: 1 + data.width,
+        y2: 1 + data.height
+      }
       self.image = data.image
-      self.imageToLoad = 'bookimages/' + self.image
+      self.imageToLoad = 'bookimages/' + self.image + '?v=' + Math.floor((Math.random() * 1000) + 1)
       self.options = {
         parent: '.modal-body',
         enable: true,
