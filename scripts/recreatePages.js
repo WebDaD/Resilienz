799f24570643490c90923eec21d38cdf9f58cd9e
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-03 16:14:21 +0200

    script debugging

--- scripts/recreatePages.js
+++ scripts/recreatePages.js
@@ -16,28 +16,33 @@ database.getActionList(function (error, actions) {
     console.log('Got ' + actionCounter + ' Actions')
     for (let index = 0; index < actionCounter; index++) {
       const action = actions[index]
-      var pages = database.getActionPagesSync(action.id)
-      console.log('Got ' + pages.length + ' pages for action ' + action.id)
-      if (pages.length > 0) {
-        for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-          const page = pages[pageIndex]
-          var category = database.getCategoryByPageSync(page)
-          var result = books.createPage(action.id, category.id, page, action.language, '-2', false)
-          if (result) {
-            console.log('Page ' + page + ' Done')
+      try {
+        var pages = database.getActionPagesSync(action.id)
+        console.log('Got ' + pages.length + ' pages for action ' + action.id)
+        if (pages.length > 0) {
+          for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+            const page = pages[pageIndex]
+            var category = database.getCategoryByPageSync(page)
+            var result = books.createPage(action.id, category.id, page, action.language, '-2', false)
+            if (result) {
+              console.log('Page ' + page + ' Done')
+            }
           }
+          books.makeBook(action.id, action.language, function (error, book) {
+            if (error) {
+              console.error(error)
+            } else {
+              console.log('Book ' + book + ' done.')
+            }
+            bookCounter++
+            if (actionCounter === bookCounter) {
+              console.log('done')
+            }
+          })
         }
-        books.makeBook(action.id, action.language, function (error, book) {
-          if (error) {
-            console.error(error)
-          } else {
-            console.log('Book ' + book + ' done.')
-          }
-          bookCounter++
-          if (actionCounter === bookCounter) {
-            console.log('done')
-          }
-        })
+      } catch (e) {
+        console.error(error)
+        process.exit(2)
       }
     }
   }
