aa3adaf852522f530701f14bcbcbba53d5948751
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-03 16:11:13 +0200

    script fixing

--- scripts/recreatePages.js
+++ scripts/recreatePages.js
@@ -1,5 +1,4 @@
 var config = require('../config.json')
-var Sync = require('sync')
 var Database = require('../lib/database.js')
 var Books = require('../lib/books.js')
 var database = new Database(config.database)
@@ -12,44 +11,34 @@ database.getActionList(function (error, actions) {
     console.error(error)
     process.exit(2)
   } else {
-    console.log('Got ' + actions.length + ' Actions')
-    for (let index = 0; index < actions.length; index++) {
+    var actionCounter = actions.length
+    var bookCounter = 0
+    console.log('Got ' + actionCounter + ' Actions')
+    for (let index = 0; index < actionCounter; index++) {
       const action = actions[index]
-      Sync(function () {
-        try {
-          var pages = database.getActionPages.sync(action.id)
-          console.log('Got ' + pages.length + ' pages for action ' + action.id)
-          if (pages.length > 0) {
-            for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-              const page = pages[pageIndex]
-              try {
-                var category = database.getCategoryByPage.sync(page)
-                try {
-                  books.createPage.sync(action.id, category.id, page, action.language, '-2', false)
-                  console.log('Page ' + page + ' created.')
-                } catch (error) {
-                  console.error(error)
-                  process.exit(2)
-                }
-              } catch (error) {
-                console.error(error)
-                process.exit(2)
-              }
-              try {
-                var book = books.makeBook.sync(action.id, action.language)
-                console.log('Book ' + book + ' created.')
-              } catch (error) {
-                console.error(error)
-                process.exit(2)
-              }
-            }
+      var pages = database.getActionPagesSync(action.id)
+      console.log('Got ' + pages.length + ' pages for action ' + action.id)
+      if (pages.length > 0) {
+        for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+          const page = pages[pageIndex]
+          var category = database.getCategoryByPageSync(page)
+          var result = books.createPage(action.id, category.id, page, action.language, '-2', false)
+          if (result) {
+            console.log('Page ' + page + ' Done')
           }
-        } catch (error) {
-          console.error(error)
-          process.exit(2)
         }
-      })
+        books.makeBook(action.id, action.language, function (error, book) {
+          if (error) {
+            console.error(error)
+          } else {
+            console.log('Book ' + book + ' done.')
+          }
+          bookCounter++
+          if (actionCounter === bookCounter) {
+            console.log('done')
+          }
+        })
+      }
     }
-    console.log('DONE')
   }
 })
