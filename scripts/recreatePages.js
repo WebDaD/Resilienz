fdd854f56eda664e4636f857d356498c0556168d
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-03 16:26:31 +0200

    split script into two

--- scripts/recreatePages.js
+++ scripts/recreatePages.js
@@ -4,46 +4,67 @@ var Books = require('../lib/books.js')
 var database = new Database(config.database)
 var books = new Books(database, config.layouts, config.pages, config.images, config.books)
 
-console.log('STC :: Recreating All Pages and Books')
+console.log('STC :: Recreating All Pages or Books')
 
-database.getActionList(function (error, actions) {
-  if (error) {
-    console.error(error)
-    process.exit(2)
-  } else {
-    var actionCounter = actions.length
-    var bookCounter = 0
-    console.log('Got ' + actionCounter + ' Actions')
-    for (let index = 0; index < actionCounter; index++) {
-      const action = actions[index]
-      try {
-        var pages = database.getActionPagesSync(action.id)
-        console.log('Got ' + pages.length + ' pages for action ' + action.id)
-        if (pages.length > 0) {
-          for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-            const page = pages[pageIndex]
-            var category = database.getCategoryByPageSync(page)
-            var result = books.createPage(action.id, category.id, page, action.language, '-2', false)
-            if (result) {
-              console.log('Page ' + page + ' Done')
+if (process.argv[1] === 'pages') {
+  database.getActionList(function (error, actions) {
+    if (error) {
+      console.error(error)
+      process.exit(2)
+    } else {
+      var actionCounter = actions.length
+      var bookCounter = 0
+      console.log('Got ' + actionCounter + ' Actions')
+      for (let index = 0; index < actionCounter; index++) {
+        const action = actions[index]
+        database.getActionPages(action.id, function (error, pages) {
+          if (error) {
+            console.error(error)
+          } else {
+            console.log('Got ' + pages.length + ' pages for action ' + action.id)
+            if (pages.length > 0) {
+              for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+                const page = pages[pageIndex]
+                database.getCategoryByPage(page, function(error, category) {
+                  if (error) {
+                    console.error(error)
+                  } else {
+                    books.createPage(action.id, category.id, page, action.language, '-2', false, function(error, result) {
+                      if(error) {
+                        console.error(error)
+                      } else {
+                        console.log('Page ' + page + ' Done')
+                      }
+                    })
+                  }
+                })
+              }
             }
           }
-          books.makeBook(action.id, action.language, function (error, book) {
-            if (error) {
-              console.error(error)
-            } else {
-              console.log('Book ' + book + ' done.')
-            }
-            bookCounter++
-            if (actionCounter === bookCounter) {
-              console.log('done')
-            }
-          })
-        }
-      } catch (error) {
-        console.error(error)
-        process.exit(2)
+        })
       }
     }
-  }
-})
+  })
+} else {
+  database.getActionList(function (error, actions) {
+    if (error) {
+      console.error(error)
+      process.exit(2)
+    } else {
+      var actionCounter = actions.length
+      var bookCounter = 0
+      console.log('Got ' + actionCounter + ' Actions')
+      books.makeBook(action.id, action.language, function (error, book) {
+        if (error) {
+          console.error(error)
+        } else {
+          console.log('Book ' + book + ' done.')
+        }
+        bookCounter++
+        if (actionCounter === bookCounter) {
+          console.log('done')
+        }
+      })
+    }
+  })
+}
