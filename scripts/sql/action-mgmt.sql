213b59ebc5d4d1bb8def5b558446bc1716831fd7
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-05 12:26:45 +0200

    better display

--- scripts/sql/action-mgmt.sql
+++ scripts/sql/action-mgmt.sql
@@ -1,4 +1,4 @@
-INSERT INTO `strings` (`id`, `description`, `string_key`) VALUES (NULL, 'Navigation Title Manager', 'nav_manage'), (NULL, 'Title Manager', 'manage_title'), (NULL, 'Instructions for Manual', 'manage_text'),(NULL, 'Verwaltung :: Aktiv', 'manage_active'), (NULL, 'Verwaltung Kommentar zur Aktion', 'manage_comment'), (NULL, 'Verwaltung Letze Änderung der Aktion', 'manage_last_change'), (NULL, 'Verwaltung Button Erstelle Buch', 'manage_create_book'), (NULL, 'Verwaltung Information Buch wird erstellt', 'manage_creating'), (NULL, 'Verwaltung Buch downloaden', 'manage_download'), (NULL, 'Verwaltung Button Aktion wechseln', 'manage_switch_action'), (NULL, 'Verwaltung Button Aktion erstellen', 'manage_create_action');
+INSERT INTO `strings` (`id`, `description`, `string_key`) VALUES (NULL, 'Navigation Title Manager', 'nav_manage'), (NULL, 'Title Manager', 'manage_title'), (NULL, 'Instructions for Manual', 'manage_text'),(NULL, 'Verwaltung :: Aktiv', 'manage_active'), (NULL, 'Verwaltung Kommentar zur Aktion', 'manage_comment'), (NULL, 'Verwaltung Letze Änderung der Aktion', 'manage_last_change'), (NULL, 'Verwaltung Button Erstelle Buch', 'manage_create_book'), (NULL, 'Verwaltung Information Buch wird erstellt', 'manage_creating'), (NULL, 'Verwaltung Buch downloaden', 'manage_download'), (NULL, 'Verwaltung Button Aktion wechseln', 'manage_switch_action'), (NULL, 'Verwaltung Button Aktion erstellen', 'manage_create_action'), NULL, 'Verwaltung Kein Kommentar angegeben', 'manage_no_comment');
 
 
 INSERT INTO `translations` (`languages_id`, `strings_id`, `translation`) VALUES ('1', '178', 'Verwaltung'), ('1', '179', 'Verwaltung'), ('1', '180', 'Hier können Sie die Aktionen wechseln, Bücher erstellen lassen oder diese Herunterladen.'), ('2', '178', 'Management'), ('2', '179', 'Management'), ('2', '180', 'YOu can switch books, create PDFs or download them.'), ('3', '178', 'administración'), ('3', '179', 'administración'), ('3', '180', 'Aquí puede cambiar acciones, tener libros creados o descargarlos.'), ('4', '178', 'إدارة'), ('4', '179', 'إدارة'), ('4', '180', 'هنا يمكنك تغيير الإجراءات ، أو إنشاء كتب أو تنزيلها.'),('1', '181', 'Aktiv'),
@@ -32,7 +32,11 @@ INSERT INTO `translations` (`languages_id`, `strings_id`, `translation`) VALUES 
 ('4', '185', 'يتم إنشاء الكتاب ...'),
 ('4', '186', 'قم بتنزيل الكتاب'),
 ('4', '187', 'التحول إلى العمل'),
-('4', '188', 'عمل جديد');
+('4', '188', 'عمل جديد'),
+('1', '189', 'Kein Kommentar vorhanden.'),
+('2', '189', 'No comment available.'),
+('3', '189', 'No hay comentarios disponibles'),
+('4', '189', 'لا تعليق متاح.');
 
 ALTER TABLE `actions` ADD `comment` TEXT NULL AFTER `book`, ADD `last_change` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `comment`, ADD `active` TINYINT NOT NULL DEFAULT '0' AFTER `last_change`;
 
