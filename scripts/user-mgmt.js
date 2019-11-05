4139b0bd514bb662b09e24124c36607654aa9411
Dominik Sigmund <dominik.sigmund@br.de>
2019-11-05 14:58:59 +0100

    Added User mgmt

---
+++ scripts/user-mgmt.js
@@ -0,0 +1,30 @@
+const mode = process.argv[1]
+
+var config = require('../config.json')
+var Database = require('../lib/database.js')
+
+var database = new Database(config.database)
+
+if (mode === 'delete') {
+  var userID = process.argv[2]
+  var actionID = process.argv[3]
+  database.deleteUser(userID, actionID, function(status, error) {
+    if (error) {
+      console.error(error)
+    } else {
+      console.log(status)
+    }
+  })
+} else { // list users
+  database.getUsers(function(users, error) {
+    if (error) {
+      console.error(error)
+    } else {
+      console.log('-- users --')
+      console.log('-----------')
+      users.forEach(user => {
+        console.log(user.id + ' - ' + user.action_id + ' : ' + user.vorname + ' ' + user.nachname + ' <' + user.email + '>')
+      })
+    }
+  })
+}
